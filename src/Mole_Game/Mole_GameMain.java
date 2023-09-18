9501faf79af9bb19d43d90fd66b053e6bb933d35
Kim Yongjin <zkdlwls0812@gmail.com>
2023-09-19 00:00:48 +0900

    Many moles and cooldown implement

--- src/Mole_Game/Mole_GameMain.java
+++ src/Mole_Game/Mole_GameMain.java
@@ -4,21 +4,42 @@ import java.awt.*;
 import java.awt.event.*;
 import java.util.*;
 import javax.swing.*;
-import javax.swing.Timer;
 
 public class Mole_GameMain extends JFrame implements ActionListener {
     static final int WIDTH = 1024;
     static final int HEIGHT = 720;
     private JLabel label1, label2;
+    Hammer1p hammer1p = new Hammer1p(120, 250);
     Hammer2p hammer2p = new Hammer2p(720, 250);
-    Normal_Mole Normal_Mole = new Normal_Mole((int) (Math.random() * (Mole_GameMain.WIDTH-100)),
-            (int) (Math.random() * (Mole_GameMain.HEIGHT-115)));
+    public ArrayList<Normal_Mole> normal_mole_list = new ArrayList<Normal_Mole>(5);
+
     class MyPanel extends JPanel {
         public MyPanel() {
-            Runnable task = () -> {
+            Runnable mole = () -> {
                 while (true) {
-                    if((Normal_Mole.x <= hammer2p.x && hammer2p.x <= Normal_Mole.x+100)&&(Normal_Mole.y <= hammer2p.y+100 && hammer2p.y+100 <= Normal_Mole.y+115)&& hammer2p.smash_state){
-                        Normal_Mole.update((int) (Math.random() * (Mole_GameMain.WIDTH-100)), (int) (Math.random() * (Mole_GameMain.HEIGHT-115)));
+                    for(int i=0; i<normal_mole_list.size(); i++) {
+                        normal_mole_list.get(i).time_decrease();
+                    }
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException ignore) {
+                    }
+                }
+            };
+            Runnable hammer = () -> {
+                for(int i=0; i<5; i++) {
+                    normal_mole_list.add(new Normal_Mole((int) (Math.random() * (Mole_GameMain.WIDTH-100)),
+                            (int) (Math.random() * (Mole_GameMain.HEIGHT-115)), (int) (Math.random() * 7 + 3)));
+                }
+
+                while (true) {
+                    for(int i=0; i<normal_mole_list.size(); i++) {
+                        if((normal_mole_list.get(i).x <= hammer2p.x && hammer2p.x <= normal_mole_list.get(i).x+100)&&(normal_mole_list.get(i).y <= hammer2p.y+100 && hammer2p.y+100 <= normal_mole_list.get(i).y+115)&& hammer2p.smash_state){
+                            normal_mole_list.get(i).update_by_smash((int) (Math.random() * (Mole_GameMain.WIDTH-100)), (int) (Math.random() * (Mole_GameMain.HEIGHT-115)));
+                        }
+                        if(normal_mole_list.get(i).cooldown == 0) {
+                            normal_mole_list.get(i).update_by_time_reset((int) (Math.random() * (Mole_GameMain.WIDTH-100)), (int) (Math.random() * (Mole_GameMain.HEIGHT-115)), (int) (Math.random() * 7 + 3));
+                        }
                     }
                     repaint();
                     try {
@@ -27,11 +48,14 @@ public class Mole_GameMain extends JFrame implements ActionListener {
                     }
                 }
             };
-            new Thread(task).start();
+            new Thread(mole).start();
+            new Thread(hammer).start();
         }
         public void paintComponent(Graphics g) {
             super.paintComponent(g);
-            Normal_Mole.draw(g);
+            for (Normal_Mole nm : normal_mole_list) {
+                nm.draw(g);
+            }
             hammer2p.draw(g);
         }
     }
--- src/Mole_Game/Normal_Mole.java
+++ src/Mole_Game/Normal_Mole.java
@@ -4,12 +4,13 @@ import javax.swing.*;
 import java.awt.*;
 
 public class Normal_Mole {
-    int x, y;
+    int x, y, cooldown;
     Image img;
 
-    Normal_Mole(int x, int y) {
+    Normal_Mole(int x, int y, int cooldown) {
         this.x = x;
         this.y = y;
+        this.cooldown = cooldown;
         ImageIcon icon = new ImageIcon("../Mole_Game/src/Mole_Game/Mole.png");
         img = icon.getImage();
     }
@@ -18,7 +19,17 @@ public class Normal_Mole {
         g.drawImage(img, x, y, null);
     }
 
-    public void update(int x, int y) {
+    public void time_decrease() {
+        this.cooldown --;
+    }
+
+    public void update_by_time_reset(int x, int y, int time) {
+        this.x = x;
+        this.y = y;
+        this.cooldown = time;
+    }
+
+    public void update_by_smash(int x, int y) {
         this.x = x;
         this.y = y;
     }

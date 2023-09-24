d85665032ffdb6458c3011c3df8a78495e709497
Kim Yongjin <zkdlwls0812@gmail.com>
2023-09-25 00:51:54 +0900

    두더지 마릿수를 5마리에서 3마리로 감소가 안 되는 현상 수정, 점수 구현 및 라벨 추가

--- src/Mole_Game/Mole_GameMain.java
+++ src/Mole_Game/Mole_GameMain.java
@@ -8,10 +8,13 @@ import javax.swing.*;
 public class Mole_GameMain extends JFrame implements ActionListener {
     static final int WIDTH = 1024;
     static final int HEIGHT = 720;
-    private JLabel label1, label2;
-    Hammer1p hammer1p = new Hammer1p(120, 250);
-    Hammer2p hammer2p = new Hammer2p(720, 250);
-    public ArrayList<Normal_Mole> normal_mole_list = new ArrayList<Normal_Mole>(3);
+    static final int MOLE_COUNT = 3;
+    private final JLabel label1, label2, label3;
+    public ArrayList<Normal_Mole> normal_mole_list = new ArrayList<Normal_Mole>(MOLE_COUNT);
+
+    static Hammer1p hammer1p = new Hammer1p(120, 250);
+    static Hammer2p hammer2p = new Hammer2p(720, 250);
+    static int score_1p = 0, score_2p = 0;
 
     class MyPanel extends JPanel {
         public MyPanel() {
@@ -27,13 +30,13 @@ public class Mole_GameMain extends JFrame implements ActionListener {
                 }
             };
             Runnable hammer = () -> {
-                for(int i=0; i<5; i++) {
+                for(int i=0; i<MOLE_COUNT; i++) {
                     normal_mole_list.add(new Normal_Mole((int) (Math.random() * (Mole_GameMain.WIDTH-100)),
                             (int) (Math.random() * (Mole_GameMain.HEIGHT-115)), (int) (Math.random() * 7 + 3)));
                 }
-
                 while (true) {
                     for(int i=0; i<normal_mole_list.size(); i++) {
+
                         if(normal_mole_list.get(i).cooldown == 0) {
                             normal_mole_list.get(i).update_by_time_reset((int) (Math.random() * (Mole_GameMain.WIDTH-200) + 100), (int) (Math.random() * (Mole_GameMain.HEIGHT-150) + 80), (int) (Math.random() * 7 + 3));
                         }
@@ -67,11 +70,22 @@ public class Mole_GameMain extends JFrame implements ActionListener {
     public Mole_GameMain() {
         MyPanel panel = new MyPanel();
         panel.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+        setTitle("Mole Game");
         add(panel);
         pack();
+        label1 = new JLabel("1p Score : " + score_1p);
+        label2 = new JLabel("2p Score : " + score_2p);
+        label3 = new JLabel();
+        label1.setLocation(0, 0);
+        label2.setLocation(1000, 0);
+        panel.add(label1);
+        panel.add(label2);
+        panel.add(label3);
         setVisible(true);
-        panel.requestFocus();
         panel.setFocusable(true);
+        panel.requestFocus();
+
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
 
         panel.addKeyListener(new KeyAdapter() {
             public void keyPressed(KeyEvent e) {
@@ -110,18 +124,21 @@ public class Mole_GameMain extends JFrame implements ActionListener {
             public void keyReleased(KeyEvent e) {
                 if(e.getKeyCode() == KeyEvent.VK_CONTROL) {
                     hammer1p.return_hammer();
+                    score_1p += 10;
+                    label1.setText("1p Score : " + score_1p);
                 }
                 if(e.getKeyCode() == KeyEvent.VK_SLASH) {
                     hammer2p.return_hammer();
+                    score_2p += 10;
+                    label2.setText("2p Score : " + score_2p);
                 }
             }
         });
-
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
     }
 
     public void actionPerformed(ActionEvent e) {
-        //repaint();
+
+
     }
 
     public static void main(String[] args) {
